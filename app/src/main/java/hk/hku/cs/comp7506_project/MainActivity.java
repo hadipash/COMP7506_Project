787962cdea38fabcae699e4d0289833cd5d16f13
Rustam <dknext99@gmail.com>
2018-11-01 12:47:56 +0800

    Initial commit

---
+++ app/src/main/java/hk/hku/cs/comp7506_project/MainActivity.java
@@ -0,0 +1,13 @@
+package hk.hku.cs.comp7506_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
